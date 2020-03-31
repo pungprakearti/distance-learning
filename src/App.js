9ce74988dbd10a7160b2c71ddf0a2ca3ec22036b
Andrew Pungprakearti <andrew@alephsf.com>
2020-03-31 12:35:42 -0700

    Add github link and pointer for buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import DistanceLearning from './DistanceLearning'
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <DistanceLearning />
+      <a className='App_Link' href='https://github.com/pungprakearti/distance-learning'>Github repository</a>
     </div>
   );
 }
